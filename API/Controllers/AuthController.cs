7cdb336db49c4a0ead9743d06123d9b40f3d7f25
daviti <davitigg@gmail.com>
2023-01-07 11:09:48 +0400

    Add project files.

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // fake database    
+        private static readonly List<LoginModel> users = new()
+        {
+            new LoginModel("admin", "admin"),
+            new LoginModel("test", "test"),
+            new LoginModel("string", "string") };
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel user)
+        {
+            if (user is null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            if (users.Contains(user))
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:5001",
+                    audience: "https://localhost:5001",
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signinCredentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+
+                return Ok(new AuthenticatedResponse { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ API/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Models
+{
+    public class LoginModel : IEquatable<LoginModel>
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+
+        public LoginModel(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+
+        public bool Equals(LoginModel? other)
+        {
+            return UserName == other.UserName && Password == other.Password;
+        }
+    }
+}
